0479c5b63c6079e544f84bb57be2e7eaba9d2696
miracle <miracle@fedora.www.tendawifi.com>
2024-05-16 03:37:33 -0400

    reconnect

---
+++ src/components/Provider.tsx
@@ -0,0 +1,9 @@
+// "use client";
+// import React from "react";
+// import { QueryClientProvider } from "react-query";
+
+// const Provider = () => {
+//   return <QueryClientProvider>Provider</QueryClientProvider>;
+// };
+
+// export default Provider;
