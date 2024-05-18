8a1fc5068bebb3c1151b209bc7400cc9d965b261
Miracle <Miracleiztb@gmail.com>
2024-05-18 20:52:15 +0700

    fix

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { HTMLAttributes, useEffect, useRef, useState } from "react";
@@ -161,6 +162,7 @@ export default function Reviews() {
         aria-hidden="true"
         src="/what-people-are-buying.png"
         className="absolute select-none hidden xl:block -left-32 top-1/3"
+        alt="What people are buying"
       />
 
       <ReviewGrid />
