a638abc0558c7e965c7f0aee6d3bcd3beec0cd74
Miracle <Miracleiztb@gmail.com>
2024-05-18 21:57:35 +0700

    fix_v4

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,37 +1,121 @@
-import { db } from "@/db"
-import { stripe } from "@/lib/stripe"
-import { headers } from "next/headers"
-import { NextResponse } from "next/server"
-import Stripe from "stripe"
+// import { db } from "@/db"
+// import { stripe } from "@/lib/stripe"
+// import { headers } from "next/headers"
+// import { NextResponse } from "next/server"
+// import Stripe from "stripe"
+
+// export async function POST(req: Request) {
+//     try {
+//         const body = await req.text()
+//         const signature = headers().get("stripe-signature")
+
+//         if (!signature) return new Response("Invalid signature", { status: 400 })
+
+//         const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!)
+
+//         if (event.type === "checkout.session.completed") {
+//             if (!event.data.object.customer_details?.email) throw new Error("Missing user email")
+
+//             // Do something with the event
+//             const session = event.data.object as Stripe.Checkout.Session
+
+//             const { userId, orderId } = session.metadata || {
+//                 userId: null,
+//                 orderId: null
+//             }
+
+//             if (!userId || !orderId) throw new Error("Invalid metadata")
+
+//             const billingAddress = session.customer_details?.address
+//             const shippingAddress = session.shipping_details?.address
+
+//             await db.order.update({
+//                 where: {
+//                     id: orderId
+//                 },
+//                 data: {
+//                     isPaid: true,
+//                     shippingAddress: {
+//                         create: {
+//                             name: session.customer_details!.name!,
+//                             city: shippingAddress!.city!,
+//                             country: shippingAddress!.country!,
+//                             postalCode: shippingAddress!.postal_code!,
+//                             street: shippingAddress!.line1!,
+//                             state: shippingAddress!.state,
+//                         },
+//                     },
+//                     billingAddress: {
+//                         create: {
+//                             name: session.customer_details!.name!,
+//                             city: billingAddress!.city!,
+//                             country: billingAddress!.country!,
+//                             postalCode: billingAddress!.postal_code!,
+//                             street: billingAddress!.line1!,
+//                             state: billingAddress!.state,
+//                         },
+//                     },
+//                 },
+//             })
+//         }
+//         return NextResponse.json({
+//             result: event,
+//             ok: true
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         // send this to sentry
+
+//         return NextResponse.json({
+//             message: "Something went wrong",
+//             ok: false
+//         }, { status: 500 })
+//     }
+// }
+
+import { db } from '@/db'
+import { stripe } from '@/lib/stripe'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
 
 export async function POST(req: Request) {
     try {
         const body = await req.text()
-        const signature = headers().get("stripe-signature")
+        const signature = headers().get('stripe-signature')
 
-        if (!signature) return new Response("Invalid signature", { status: 400 })
+        if (!signature) {
+            return new Response('Invalid signature', { status: 400 })
+        }
 
-        const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!)
+        const event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
 
-        if (event.type === "checkout.session.completed") {
-            if (!event.data.object.customer_details?.email) throw new Error("Missing user email")
+        if (event.type === 'checkout.session.completed') {
+            if (!event.data.object.customer_details?.email) {
+                throw new Error('Missing user email')
+            }
 
-            // Do something with the event
             const session = event.data.object as Stripe.Checkout.Session
 
             const { userId, orderId } = session.metadata || {
                 userId: null,
-                orderId: null
+                orderId: null,
             }
 
-            if (!userId || !orderId) throw new Error("Invalid metadata")
+            if (!userId || !orderId) {
+                throw new Error('Invalid request metadata')
+            }
 
-            const billingAddress = session.customer_details?.address
-            const shippingAddress = session.shipping_details?.address
+            const billingAddress = session.customer_details!.address
+            const shippingAddress = session.shipping_details!.address
 
-            await db.order.update({
+            const updatedOrder = await db.order.update({
                 where: {
-                    id: orderId
+                    id: orderId,
                 },
                 data: {
                     isPaid: true,
@@ -58,17 +142,14 @@ export async function POST(req: Request) {
                 },
             })
         }
-        return NextResponse.json({
-            result: event,
-            ok: true
-        })
-    } catch (error) {
-        console.log(error);
-        // send this to sentry
 
-        return NextResponse.json({
-            message: "Something went wrong",
-            ok: false
-        }, { status: 500 })
+        return NextResponse.json({ result: event, ok: true })
+    } catch (err) {
+        console.error(err)
+
+        return NextResponse.json(
+            { message: 'Something went wrong', ok: false },
+            { status: 500 }
+        )
     }
 }
